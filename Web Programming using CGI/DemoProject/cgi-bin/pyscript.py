5a5cab46eee54c920ef50b6cae3cf7e5be226bef
Sara Bhanu Prasad Goud <154022197+bhanuprasadgoudsara@users.noreply.github.com>
2024-03-12 11:41:00 +0530

    Add files via upload

---
+++ Web Programming using CGI/DemoProject/cgi-bin/pyscript.py
@@ -0,0 +1,11 @@
+# Command Prompt Commnd
+# python -m http.server --cgi 8080
+
+#!C:\Python34\python.exe
+import cgi
+print('Content-type:text/html')
+print()
+print('''
+<html> <head></head>
+<body></body> </html>
+''')
